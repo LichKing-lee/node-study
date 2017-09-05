be42fd9e342ae76b299298bbb78aab2f03cc8395
ChangYong <lcy9002@naver.com>
2017-09-05 22:22:43 +0900

    view template pug

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const pug = require("pug");
+
+app.locals.pretty = true;
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-    res.send("Hello NodeJS");
+    res.render("first", {
+        variable1: "Hello Variable"
+    });
 });
 
 app.listen(3000, () => console.log("Hello NodeJS"));
